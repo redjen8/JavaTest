ed1abbeda88bd093d5f9bf2455a05fd6383e93a0
Seokwoo Jeong <seokwoo2525@gmail.com>
2022-01-17 14:30:24 +0900

    added missing sharedMemoryTest

---
+++ ShareMemoryTest/Main.java
@@ -0,0 +1,10 @@
+import ShareMemoryTest.ShareMemoryTest;
+
+class Main {
+    public static void main(String[] args) {
+        ShareMemoryTest sm = new ShareMemoryTest();
+        sm.eachListTest1();
+        sm.eachListTest2();
+        System.out.println("Hello, world!");
+    }
+}
